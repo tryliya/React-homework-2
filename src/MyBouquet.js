611987567ee9c50689a1954bc3717661ab1d4340
tryliya <tryliya@gmail.com>
2021-07-09 15:09:58 +0300

    add MyBouquet file

---
+++ src/MyBouquet.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class MyBouquet extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Составьте Ваш собственный букет</p>
+
+            </div>
+        )
+    }
+}
+
+
+export default MyBouquet;
